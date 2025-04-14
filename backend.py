a1fa30a37c8d825d84e53b791f368b31cc005759
Elfsong <dumingzhe@126.com>
2025-04-14 06:57:49 +0000

    Update

--- backend.py
+++ backend.py
@@ -9,58 +9,42 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 # Connect to local Redis server
-redis_client = redis.Redis(host='localhost', port=6379, db=0)
-
-@app.route("/kill_process", methods=["POST"])
-def kill_process():
-    data = request.get_json()
-    # TODO(Andrew): Validate data / Authorization
-    
-    if not data:
-        return jsonify({"status": "error", "message": "No JSON data"}), 400
-
-    if "sid" not in data:
-        return jsonify({"status": "error", "message": "No SID provided"}), 400
-
-    if "pid" not in data:
-        return jsonify({"status": "error", "message": "No PID provided"}), 400
-    
-    # Get Metadata  
-    pid = data["pid"]
-    sid = data["sid"]
-
-    # Kill Process
-    try:
-        os.kill(pid, signal.SIGKILL)
-        print(f"[✔] Killed PID {pid}")
-        return jsonify({"status": "ok"}), 200
-    except ProcessLookupError:
-        return jsonify({"status": "error", "message": "PID does not exist"}), 400
-    except PermissionError:
-        return jsonify({"status": "error", "message": "No permission to kill PID"}), 500
-    except Exception as e:
-        return jsonify({"status": "error", "message": "Failed to kill PID"}), 500
-    
+redis_client = redis.Redis(host='localhost', port=6379, password=os.getenv("REDIS_PASSWORD"), db=0)
 
 @app.route("/submit_gpu_data", methods=["POST"])
 def post_gpu_data():
     data = request.get_json()
     # TODO(Andrew): Validate data / Authorization
+
     if not data:
         return jsonify({"status": "error", "message": "No JSON data"}), 400
+    if "sid" not in data:
+        return jsonify({"status": "error", "message": "No SID provided"}), 400
+    if "system_data" not in data:
+        return jsonify({"status": "error", "message": "No GPU data provided"}), 400
+    
+    system_data = data["system_data"]
 
-    redis_client.set("gpu_data", json.dumps(data))
+    redis_client.set(f"{data['sid']}_data", json.dumps(system_data))
     return jsonify({"status": "ok"}), 200
 
 @app.route("/get_gpu_data", methods=["GET"])
 def get_gpu_data():
+    data = request.get_json()
     # TODO(Andrew): Validate data / Authorization
-    data = redis_client.get("gpu_data")
-    if data:
-        return jsonify(json.loads(data))
+
+    if not data:
+        return jsonify({"status": "error", "message": "No GPU data found"}), 404
+    if "sid" not in data:
+        return jsonify({"status": "error", "message": "No SID provided"}), 400
+
+    system_data = redis_client.get(f"{data['sid']}_data")
+
+    if system_data:
+        return jsonify(json.loads(system_data))
     else:
         return jsonify({"status": "error", "message": "No GPU data found"}), 404
 
 if __name__ == "__main__":
+    # TODO(Andrew): Wrap it using gunicorn
     app.run(host="0.0.0.0", port=8000)
-
--- frontend.py
+++ frontend.py
@@ -1,7 +1,17 @@
 # coding: utf-8
 
+import os
+import redis
 import requests
 import streamlit as st
 
+# Initialize Redis Client
+redis_client = redis.Redis(host='localhost', port=6379, password=os.getenv("REDIS_PASSWORD"), db=0)
+
 st.title("Argus")
-st.balloons()
+
+# Get GPU Data
+response = requests.get("http://localhost:8000/get_gpu_data")
+st.write(response.json())
+
+

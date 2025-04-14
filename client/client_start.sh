f91fc74ec8e3bbf77d3c0921fe348280c673b937
Elfsong <dumingzhex@gmail.com>
2025-04-14 17:36:11 +0800

    Update

--- client/client_start.sh
+++ client/client_start.sh
@@ -1,1 +1,20 @@
+#! /bin/bash
+
+# Create virtual environment
+echo "Creating virtual environment..."
+if [ ! -d "argus_venv" ]; then  
+    python3 -m venv argus_venv
+fi
+
+# Activate virtual environment
+echo "Activating virtual environment..."
+source argus_venv/bin/activate
+
+# Install dependencies
+echo "Installing dependencies..."
+pip install requests psutil flask python-daemon
+
+# Start the daemon
+echo "Starting the daemon..."
 python argus_daemon.py
+echo "Daemon started successfully."
